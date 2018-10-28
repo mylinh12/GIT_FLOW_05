df149de68c5fa29a2d108694e0f598ed14f5b336
LAPTOP A <laptop_a@gmail.com>
2018-10-28 09:59:37 +0700

    Finish TC_02_CheckTitle

--- src/selenium/TC_01_GitFlow.java
+++ src/selenium/TC_01_GitFlow.java
@@ -1,14 +1,16 @@
 package selenium;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
 
 public class TC_01_GitFlow {
-	
-  @Test
-  public void TC_01() {
-	  
-  }
-  
+
+	WebDriver driver;
+
+	@Test
+	public void TC_01() {
+		driver = new FirefoxDriver();
+	}
+
 }
---
+++ src/selenium/TC_02_CheckTitle.java
@@ -0,0 +1,14 @@
+package selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TC_02_CheckTitle {
+	
+  @Test
+  public void TC_01() {
+	  
+  }
+  
+}
