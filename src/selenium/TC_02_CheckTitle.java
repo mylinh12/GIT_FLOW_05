0ba0e7d48fdb1338c50ae1ea4a7d82f473659681
LAPTOP B <laptop_b@gmail.com>
2018-10-28 14:30:31 +0700

    Add TC_02

---
+++ src/selenium/TC_02_CheckTitle.java
@@ -0,0 +1,27 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TC_02_CheckTitle {
+	WebDriver driver;
+
+	@Test
+	public void TC_01() {
+		driver = new FirefoxDriver();
+		driver.get("http://google.com");
+		driver.manage().window().maximize();		
+		driver.findElement(By.xpath("//input[@name='q']")).clear();
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Automation Testing");
+		driver.findElement(By.xpath("//input[@name='btnK']")).click();
+		
+		driver.findElement(By.cssSelector("input[@name='q']")).sendKeys("Monkey Testing");
+		driver.findElement(By.name("q")).sendKeys("Adhoc Testing");
+		driver.findElement(By.xpath("//input[@name='btnK']")).click();
+		
+		driver.quit();
+	}
+
+}
