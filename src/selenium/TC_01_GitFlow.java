1369e1cadbf50e304a0ac816e88dae05b087b732
LAPTOP A <laptop_a@gmail.com>
2018-10-28 11:40:53 +0700

    Add Search function

--- src/selenium/TC_01_GitFlow.java
+++ src/selenium/TC_01_GitFlow.java
@@ -1,18 +1,20 @@
 package selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class TC_01_GitFlow {
-
 	WebDriver driver;
 
 	@Test
 	public void TC_01() {
 		driver = new FirefoxDriver();
 		driver.get("http://google.com");
-		driver.manage().window().maximize();
+		driver.manage().window().maximize();		
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Automation Testing");
+		driver.findElement(By.xpath("//input[@name='btnK']")).click();
 		driver.quit();
 	}
 
