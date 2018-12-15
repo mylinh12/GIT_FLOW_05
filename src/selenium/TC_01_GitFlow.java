3c6701dda36632b1e9adf28cda76dcbe94241630
LAPTOP A <laptop_a@gmail.com>
2018-12-15 22:35:20 +0700

    re-test about syncing project (3)

--- src/selenium/TC_01_GitFlow.java
+++ src/selenium/TC_01_GitFlow.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-//
 public class TC_01_GitFlow {
 	WebDriver driver;
 
